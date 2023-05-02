07577e717d607ef2fd19ff8b8c56b337eb8cf7fd
Devvrat <devvratshukla@gmail.com>
2023-05-02 23:55:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Create React Vite App
+
+This is a template for building client side react app.
+
+### TODO
+
+[-] Add support for i18n
+
+[-] Add support for e2e tests
+
+[-] Add support for writing unit tests
+
+[-] Add support for state management
+
+[-] Add support for commit linter -> https://commitlint.js.org/#/
+
+Note: This repo is currently under active development.
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <h1 className="title-text">Hello World!</h1>
+        <h2 className="title-description">
+          Powered by React + Tailwind + Vite
+        </h2>
+      </div>
+    </main>
+  );
+}
+
+export default App;
